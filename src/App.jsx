bb00ebdd38d4e498d303346768c0772c50576ea6
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-03-21 14:18:26 +0100

    Nike Landing Page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b"> Hero</section>
+    <section className="padding">Popular Products</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">Special Offer</section>
+    <section className="padding">Customer Review</section>
+    <section className="padding">Subscribe</section>
+    <section className="padding">Footer</section>
+  </main>;
+};
+
+export default App;
