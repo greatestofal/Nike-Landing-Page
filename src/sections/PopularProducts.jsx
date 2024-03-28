024f24c1f75f22fcf75803d147d4decd8b65e48e
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-03-28 13:06:20 +0100

    Almost done

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,3 +1,4 @@
+import PopularProductCard from "../Components/PopularProductCard";
 import { products } from "../constants";
 function PopularProducts() {
   return (
@@ -13,8 +14,10 @@ function PopularProducts() {
           selections. Discover a world of comfort, design, and value
         </p>
       </div>
-      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grids-cols-1 sm:gap-4 gap-14">
-        {products.map ((product) => )}
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
       </div>
     </section>
   );
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,14 @@
-function Services() {
-  return <div>Services</div>;
-}
+import ServiceCard from "../Components/ServiceCard";
+import { services } from "../constants";
+
+const Services = () => {
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
 
 export default Services;
