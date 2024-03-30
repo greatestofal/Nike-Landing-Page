2a346edc970d78f00eccde75cd9562fa47574934
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-03-30 23:29:25 +0100

    Finalized

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,4 @@
+import { copyrightSign } from "../assets/icons";
 import { footerLogo } from "../assets/images";
 import { footerLinks, socialMedia } from "../constants";
 
@@ -26,7 +27,6 @@ const Footer = () => {
           {footerLinks.map((section) => (
             <div key={section}>
               <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">
-                {" "}
                 {section.title}
               </h4>
               <ul>
@@ -42,6 +42,20 @@ const Footer = () => {
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center font-montserrat gap-2 cursor-pointer">
+          <img
+            src={copyrightSign}
+            alt="copyright sign"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p> Copyright. All rights reserved.</p>
+        </div>
+        <p className="font-montserrat cursor-pointer"> Terms & Conditions</p>
       </div>
     </footer>
   );
